0e68f8821439e4364791a606658312a9afbc12fd
Juliuuss <j.esp.ver@gmail.com>
2023-02-13 22:59:11 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,30 @@
 import React from "react";
 import {Counter} from "./counter.jsx"
 
-//include images into your bundle
-
-
-
-//create your first component
 const Home = (props) => {
 	return (
-		<div className="container-fluid border">
-			<div className="row m-3 text-center">
-				<div className="col border">
-					<h1> clock</h1>
+		<div className="container-sm px-4 text-center p-3">
+			<div className="row text-center gx-5 bg-dark p-3">
+				<div className="col">
+					<div class="p-3 border bg-dark.bg-gradient text-white fs-1 rounded"><i class="fas fa-clock"></i></div>
 				</div>
-				<div className="col border">
-					0
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter5}/></div>
 				</div>
-				<div className="col border">
-					0
-
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter4}/></div>
 				</div>
-				<div className="col border">
-					0
-
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter3}/></div>
 				</div>
-				<div className="col border">
-					0
-
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter2}/></div>
 				</div>
-				<div className="col border">
-					<Counter contador={props.counter1}/>
-
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter1}/></div>
 				</div>
-				<div className="col border">
-					<Counter contador={props.counter}/>
-
+				<div className="col ">
+					<div class="p-3 border bg-dark.bg-gradient text-white rounded"><Counter contador={props.counter}/></div>
 				</div>
 			</div>
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,29 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
+
 let counter=0;
 let counterA=0;
+let counterB=0;
+let counterC=0;
+let counterD=0;
+let counterE=0;
 
 setInterval(()=>{
-    if (counter > 9) counterA++;
-    ReactDOM.render(<Home counter={counter++} counter1={counterA} />, 
-    document.querySelector("#app"))},1000)
+    if (counter > 9) {counter = 0; counterA++}
+    if (counterA > 9) {counterA = 0; counterB++}
+    if (counterB > 9) {counterB = 0; counterC++}
+    if (counterC > 9) {counterC = 0; counterD++}
+    if (counterD > 9) {counterD = 0; counterE++}
 
 
+    ReactDOM.render(<Home
+         counter={counter++}
+         counter1={counterA}
+         counter2={counterB}
+         counter3={counterC}
+         counter4={counterD}
+         counter5={counterE} />, 
+    document.querySelector("#app"))},100)
 
-//render your react application
-ReactDOM.render(<Home counter={counter} />, document.querySelector("#app"));
